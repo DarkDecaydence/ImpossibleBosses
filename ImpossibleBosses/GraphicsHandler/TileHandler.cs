97a5b90b9e59f892ce772faea22550f69738367f
Alexander Kirk Joergensen <alkii900@msn.com>
2014-08-09 04:15:40 +0200

    First Commit

---
+++ ImpossibleBosses/GraphicsHandler/TileHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ImpossibleBosses
+{
+    class TileHandler
+    {
+        private static Dictionary<String, Texture2D> tileSets = new Dictionary<string, Texture2D>();
+
+        public static void AddTileSet(String setName, Texture2D tileSet)
+        { tileSets.Add(setName, tileSet); }
+
+        public static Texture2D GetTileSet(String setName)
+        {
+            Texture2D tileSet;
+            if (tileSets.TryGetValue(setName, out tileSet))
+            { return tileSet; }
+            else { throw new Exception("No such tile set!!"); }
+        }
+    }
+}
---
+++ ImpossibleBosses/PlayerController/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace ImpossibleBosses.PlayerController
+{
+    class PlayerController
+    {
+        private int playerNumber = 0;
+        private Dictionary<Keys, String> commandTable = new Dictionary<Keys, String>();
+
+        private void AddCommand(Keys key, String command)
+        { commandTable.Add(key, command); }
+
+        public PlayerController(int playerNumber)
+        {
+            this.playerNumber = playerNumber;
+            Keys[] playerMovement = { Keys.W, Keys.A, Keys.S, Keys.D };
+            String[] movementCommands = { "moveUp", "moveLeft", "moveDown", "moveRight" };
+            Keys[] playerActions = { Keys.Q, Keys.E, Keys.V };
+            String[] actionCommands = { "block", "attack", "special" };
+
+            for (int i = 0; i < playerMovement.Length; i++)
+            { AddCommand(playerMovement[i], movementCommands[i]); }
+        }
+
+        public Vector2 Update()
+        {
+            String command = "";
+            Vector2 tempMoveDir = new Vector2();
+            Vector2 finalMoveDir = new Vector2();
+            
+            foreach (Keys k in Keyboard.GetState().GetPressedKeys())
+            {
+                if (!commandTable.TryGetValue(k, out command))
+                { continue; }
+                tempMoveDir = ProcessCommand(command);
+                finalMoveDir += tempMoveDir;
+            }
+
+            Vector2.Clamp(finalMoveDir, new Vector2(-1, -1), new Vector2(1, 1));
+
+            return finalMoveDir;
+
+        }
+
+        private Vector2 ProcessCommand(String command)
+        {
+            switch (command)
+            {
+                case "moveUp": return new Vector2(0, -1);
+                case "moveLeft": return new Vector2(-1, 0);
+                case "moveDown": return new Vector2(0, 1);
+                case "moveRight": return new Vector2(1, 0);
+                default: return new Vector2(0, 0);
+            }
+        }
+    }
+}
